726c4926f21e329f573f207870e78e019c339454
qrak <donqrakko@gmail.com>
2021-12-24 20:17:50 +0100

    Version 1.0 - Compounding integration in progress

--- config.py
+++ config.py
@@ -1,17 +1,13 @@
-# To disable logging set it to False
+# To enable logging change to True
 LOGGING = False
 
-# Enable bot compounding and interval - NOT WORKING YET, DON'T USE!
-# BOT_COMPOUNDING = False
-# BOT_COMPOUNDING_INTERVAL = 5 #86400 seconds - 1 day
+# Configurable step scale. Default is 0.5, so every safety order hit takeprofit is increased by 0.5%
+PROFIT_STEP_SCALE = 0.5
 
 # PROFIT INCREASE MODE:
 # Change False to True if you want to set fixed take profit for every order, check PROFIT_INCREASE_STEPS
 
 FIXED_INCREASE_MODE = False
-
-# Configurable step scale. Default is 0.5, so every safety order hit takeprofit is increased by 0.5%
-PROFIT_STEP_SCALE = 0.5
 
 # BOT TAKE PROFIT + STEP IN % FOR EVERY SAFETY ORDER IF FIXED MODE IS SELECTED
 #                    SO1, SO2, SO3 etc...
@@ -24,7 +20,7 @@ TIME_INTERVAL = 360
 # bots. Format: https://3commas.io/bots/xxxxx <- where xxxxx is your bot id
 # BotIds = [111111, 222222]
 
-BOT_IDS = [231231, 312321312]
+BOT_IDS = [111111, 22222]
 
 # YOUR 3commas API KEYS
 params = {
@@ -32,3 +28,7 @@ params = {
     "apiSecret": "",
 
 }
+
+# Enable bot compounding and interval - NOT WORKING YET, DON'T USE!
+# BOT_COMPOUNDING = False
+# BOT_COMPOUNDING_INTERVAL = 5 #86400 seconds - 1 day
